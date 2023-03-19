811ab1bbecf49a3a653cab088d9ead36f430b432
Zowie Erickson <zowie.erickson@gmail.com>
2023-03-19 18:19:31 -0400

    add data file with past travel experiences

---
+++ src/data.js
@@ -0,0 +1,74 @@
+export default [
+    {
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan. 2021",
+        endDate: "24 Jan. 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        title: "Acatenango Volcano",
+        location: "Guatemala",
+        googleMapsUrl: "https://goo.gl/maps/hboGiM5D8aJBqWMs8",
+        startDate: "12 Jan. 2020",
+        endDate: "25 Jan. 2020",
+        description: "Acatenango is a stratovolcano in Guatemala, close to the city of Antigua. The volcano has two peaks, Pico Mayor and Yepocapa which is also known as Tres Hermanas. Acatenango is joined with Volcán de Fuego and collectively the volcano complex is known as La Horqueta.",
+        imageUrl: "https://source.unsplash.com/KRttQCXUjNI"
+    },
+    {
+        title: "Gardens by the Bay",
+        location: "Singapore",
+        googleMapsUrl: "https://goo.gl/maps/dgTe75PU1uLVspZW6",
+        startDate: "06 Mar. 2021",
+        endDate: "09 Mar. 2021",
+        description: "The Gardens by the Bay is a nature park spanning 101 hectares in the Central Region of Singapore, adjacent to the Marina Reservoir. The park consists of three waterfront gardens: Bay South Garden, Bay East Garden and Bay Central Garden.",
+        imageUrl: "https://source.unsplash.com/OZ2rS2zCjNo"
+    },
+    {
+        title: "Petronas Twin Towers",
+        location: "Malaysia",
+        googleMapsUrl: "https://goo.gl/maps/3evTfkGJxsKtGJcW6",
+        startDate: "05 Mar. 2021",
+        endDate: "06 Mar. 2021",
+        description: "The Petronas Twin Towers are a pair of 88-storey supertall skyscrapers in Kuala Lumpur, Malaysia, standing at 451.9 metres (1,483 feet). From 1998 to 2003, they were officially designated as the tallest buildings in the world until they were surpassed by the 2004 completion of the Taipei 101.",
+        imageUrl: "https://source.unsplash.com/KcYVMFAYDck"
+    },
+    {
+        title: "Hon Thom Cable Car",
+        location: "Vietnam",
+        googleMapsUrl: "https://goo.gl/maps/Z4dQfws1qcT22N1x8",
+        startDate: "22 May. 2020",
+        endDate: "25 May. 2020",
+        description: "The Hon Thom cable car is the longest cable car in the world spanning almost 8km over 3 to 4 islands and is located in Phu Quoc, Vietnam. The cable car takes about 15 minutes each way as it takes you from the main island of Phu Quoc to Hon Thom Nature Park on Thom Island.",
+        imageUrl: "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/79/e3/dc/sun-world-hon-thom-nature.jpg?w=1400&h=-1&s=1"
+    },
+    {
+        title: "Tikal",
+        location: "Guatemala",
+        googleMapsUrl: "https://goo.gl/maps/FJZ7v4p8BdmjoN2S6",
+        startDate: "12 Jan. 2020",
+        endDate: "25 Jan. 2020",
+        description: "Tikal is an acient Mayan citadel in the rainforests of northern Guatemala. Possibly dating to the 1st century A.D., Tikal flourished between 20 and 850 A.D. and was later abandoned.",
+        imageUrl: "https://source.unsplash.com/LQPqSjvGhss"
+    },
+    {
+        title: "Antigua",
+        location: "Guatemala",
+        googleMapsUrl: "https://goo.gl/maps/fyBmNBQetYpZizU56",
+        startDate: "12 Jan. 2020",
+        endDate: "25 Jan. 2020",
+        description: "Antigua is a small city surrounded by volcanoes in southern Guatemala. It’s renowned for its Spanish colonial buildings, many of them restored following a 1773 earthquake that ended Antigua’s 200-year reign as Guatemala’s colonial capital.",
+        imageUrl: "https://source.unsplash.com/vgBxk5QJZPo"
+    },
+    {
+        title: "Lake Atitlan",
+        location: "Guatemala",
+        googleMapsUrl: "https://goo.gl/maps/9EKrogDRrWzDvSkj7",
+        startDate: "12 Jan. 2020",
+        endDate: "25 Jan. 2020",
+        description: "Lake Atitlán is a body of water in a massive volcanic crater in Guatemala’s southwestern highlands. Ringed by steep, verdant hills, it’s known for its Mayan villages and volcanoes with striking pointed cones. The busy town of Panajachel, where vendors sell traditional textiles, is a popular gateway to the lake.",
+        imageUrl: "https://unsplash.com/photos/r6X9GPgumF0"
+    },
+]
